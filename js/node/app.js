5e84bffc53035ed80213a1b641b3695f11b0074a
Mauricio González Robles <30506793+mauriciogzrs@users.noreply.github.com>
2020-03-22 18:26:58 -0600

    Feature: Node complete lab (#2)
    Co-authored-by: Mauricio Gonzalez <mauricio.gzrs@gmail.com>

---
+++ js/node/app.js
@@ -0,0 +1,19 @@
+const Mars = require('./mars');
+
+m = new Mars;
+r1 = m.addRover('spirit');
+r2 = m.addRover('curiosity', 4, 4, 'S');
+
+r1.right();
+r1.forwards(2);
+r1.left(3);
+r1.forwards(4);
+r1.right();
+r1.backwards(3);
+
+r2.command('xrzfzzzf!@#frflfrrbfrf123flbbb');
+
+r1.done();
+r2.done();
+
+m.printGrid();
---
+++ js/node/const.js
@@ -0,0 +1,4 @@
+module.exports = {
+  'obsChar': 'O',
+  'roverChar': 'R',
+};
